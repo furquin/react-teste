2fe0dd0497e56766a2137cd3fa2478bfa6216c5b
furquin <laert.ff@gmail.com>
2021-12-08 17:54:48 -0300

    modifica local do renderWithRouter por erro no lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
-import renderWithRouter from '../components/renderWithRouter';
+import renderWithRouter from './renderWithRouter';
 
 beforeEach(() => {
   renderWithRouter(<App />);
