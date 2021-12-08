319ef7b1125c4d93d004521db336c38dd0fc04a9
furquin <laert.ff@gmail.com>
2021-12-08 10:53:40 -0300

    cria testes para verificar a existencia de links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+beforeEach(() => {
+  renderWithRouter(<App />);
+});
+
+describe('Testes no componente App.js', () => {
+  test('Aplicação contém o link Home', () => {
+    const home = screen.getByRole('link', { name: 'Home' });
+
+    expect(home).toBeDefined();
+  });
+
+  test('Aplicação contém o link About', () => {
+    const About = screen.getByRole('link', { name: 'About' });
+
+    expect(About).toBeDefined();
+  });
+
+  test('Aplicação contém o link Favorite Pokémons', () => {
+    const FavoritePokémons = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(FavoritePokémons).toBeDefined();
+  });
+});
