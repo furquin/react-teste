5573b692761b0e77c12b25bcdd66e71ddf3ca7b7
furquin <laert.ff@gmail.com>
2021-12-08 19:55:10 -0300

    cria teste para vericar H2 com frase especifica

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+beforeEach(() => {
+  render(<NotFound />);
+});
+describe('testes no componente NotFound', () => {
+  test('contém um heading h2 com o texto Page requested not found 😭', () => {
+    const heading = screen.getByRole('heading',
+      { level: 2, name: /Page requested not found/i });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
