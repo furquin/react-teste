c0d5c0048913a7d2e72975728048739daa8db73f
furquin <laert.ff@gmail.com>
2021-12-08 20:18:55 -0300

    cria teste para verificar existencia de H2 com texto especifico

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+beforeEach(() => {
+  renderWithRouter(<App />);
+});
+describe('testes componente pokedex', () => {
+  test(' contém um heading h2 com o texto Encountered pokémons', () => {
+    const heading = screen.getByRole('heading',
+      { level: 2, name: /Encountered pokémons/i });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
