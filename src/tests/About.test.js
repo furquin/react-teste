1ee344331224fb68a9729dc6109fea6ed67ad754
furquin <laert.ff@gmail.com>
2021-12-08 17:38:51 -0300

    cria teste para verificar existencia de informaçoes e heading level 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('testes no componente About', () => {
+  beforeEach(() => {
+    render(<About />);
+  });
+  test('contém as informações sobre a Pokédex', () => {
+    const infOne = screen.getByText(/application/i);
+    const infTwo = screen.getByText(/filter/i);
+
+    expect(infOne).toBeDefined();
+    expect(infTwo).toBeDefined();
+  });
+
+  test('contém um heading h2 com o texto About Pokédex', () => {
+    const heading = screen.getByRole('heading', { level: 2, name: /About Pokédex/i });
+
+    expect(heading).toBeDefined();
+  });
+});
