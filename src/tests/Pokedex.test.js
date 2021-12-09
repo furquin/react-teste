bacb237cf0a8a7b4ee2e01be7f0f97465a739abd
furquin <laert.ff@gmail.com>
2021-12-09 11:32:15 -0300

    corrige testeId para passar no avaliador e cria teste para botao de resetar filtro

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -34,8 +34,8 @@ describe('testes componente pokedex', () => {
     expect(list).toHaveLength(1);
   });
 
-  test.only('Teste se a Pokédex tem os botões de filtro.', () => {
-    const buttonAll = screen.getByTestId('all');
+  test('Teste se a Pokédex tem os botões de filtro.', () => {
+    const buttonAll = screen.getByRole('button', { name: /all/i });
     const next = screen.getByTestId('next-pokemon');
     const buttons = screen.getAllByTestId('pokemon-type-button');
     const value = 7;
@@ -54,6 +54,20 @@ describe('testes componente pokedex', () => {
   });
 
   test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    const next = screen.getByTestId('next-pokemon');
+    const buttonAll = screen.getByRole('button', { name: /all/i });
+    expect(buttonAll).toBeDefined();
 
+    userEvent.click(buttonAll);
+    const pikachu = screen.getByText(/pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+
+    userEvent.click(next);
+    const charmander = screen.getByText(/charmander/i);
+    expect(charmander).toBeInTheDocument();
+
+    userEvent.click(next);
+    const caterpie = screen.getByText(/caterpie/i);
+    expect(caterpie).toBeInTheDocument();
   });
 });
