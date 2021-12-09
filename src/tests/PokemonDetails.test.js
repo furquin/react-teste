6597e508995626af41e2962afed852f8d519754d
furquin <laert.ff@gmail.com>
2021-12-09 19:42:44 -0300

    cria teste para verificar itens especificos na pagina de detalhes

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,32 +1,25 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Pokemon from '../components/Pokemon';
+import App from '../App';
 import renderWithRouter from './renderWithRouter';
-import pokemonDetails from '../data';
 
-beforeEach(() => {
-  renderWithRouter(<Pokemon
-    pokemon={ pokemonDetails[0] }
-    isFavorite
-    showDetailsLink
-  />);
-});
-
-describe('teste no componente PokemonDetails', () => {
-  test('É renderizado card com informações específicos ', () => {
-    const name = screen.getByText(/pikachu/i);
-    expect(name).toBeDefined();
-
-    const type = screen.getByText(/Electric/i);
-    expect(type).toBeDefined();
-
-    const averageWeight = screen.getByTestId(/pokemon-weight/i);
-    expect(averageWeight).toBeDefined();
-
-    const url = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
-    const imagem = screen.getByAltText(/pikachu sprite/i);
-    expect(imagem.src).toEqual(url);
+describe('teste componente pokemonDetails', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
   });
+  test('Detalhes do Pokémon selecionado são mostradas na tela.', async () => {
+    const detail = screen.getByRole('link', { name: /more details/i });
+    expect(detail).toBeDefined();
 
+    userEvent.click(detail);
+    const pokemonDetail = screen.getByText(/Pikachu Details/i);
+    expect(pokemonDetail).toBeDefined();
+
+    const heading = screen.getByRole('heading', { level: 2, name: /Summary/i });
+    expect(heading).toBeDefined();
+
+    const paragrafo = screen.getByText(/intelligent Pokémon/i);
+    expect(paragrafo).toBeDefined();
+  });
 });
