808d208bcbcef878de1f2f69e90b9cb221e24c5d
furquin <laert.ff@gmail.com>
2021-12-09 18:25:01 -0300

    cria teste para verificar existencia de card com informaçoes

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import Pokemon from '../components/Pokemon';
+import renderWithRouter from './renderWithRouter';
+import pokemonDetails from '../data';
+
+beforeEach(() => {
+  renderWithRouter(<Pokemon
+    pokemon={ pokemonDetails[0] }
+    isFavorite
+    showDetailsLink
+  />);
+});
+
+describe('teste no componente PokemonDetails', () => {
+  test('É renderizado card com informações específicos ', () => {
+    const name = screen.getByText(/pikachu/i);
+    expect(name).toBeDefined();
+
+    const type = screen.getByText(/Electric/i);
+    expect(type).toBeDefined();
+
+    const averageWeight = screen.getByTestId(/pokemon-weight/i);
+    expect(averageWeight).toBeDefined();
+
+    const url = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+    const imagem = screen.getByAltText(/pikachu sprite/i);
+    expect(imagem.src).toEqual(url);
+  });
+});
